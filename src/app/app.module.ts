7c3088825b7cba196033e89c11320c880ae0ef2f
Foudroyant <stephanebazebibouta@gmail.com>
2022-05-19 09:15:38 +0000

    Premieère version fonctionnelle sans services workers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
+//Angular material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule,
+    MatListModule, MatInputModule, MatCheckboxModule, MatCardModule,
+    AppRoutingModule, FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
